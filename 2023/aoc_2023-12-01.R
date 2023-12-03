8187dfb1d06a0d919e8544e3290684486f9dfef4
Diarmuid Lloyd <diarmuid.lloyd@gmail.com>
2023-12-03 21:07:26 +0000

    Fix order of words tackled, but still doesn't give solution.

--- 2023/aoc_2023-12-01.R
+++ 2023/aoc_2023-12-01.R
@@ -26,26 +26,42 @@ numbers_only <- numbers_only |>
   
   # Part 2
   
-data_pt2 <- sapply(data,paste0, collapse = "")
+data_pt2 <- tibble(readr::read_table("2023/data/aoc_data_2023-12-01_pt1",col_names = FALSE) ) |> rename(data = X1)
 
 # define a function to replace text with numbers
 replace_words_with_numbers <- function(string) {
-  string <- str_replace_all(string, "one", "1") |>
-    str_replace_all("two", "2") |>
-    str_replace_all("three", "3") |>
-    str_replace_all("four", "4") |>
-    str_replace_all("five", "5") |>
-    str_replace_all("six", "6") |>
-    str_replace_all("seven", "7") |>
-    str_replace_all("eight", "8") |>
-    str_replace_all("nine", "9")
+  
+  # Find the first and last instance of a number word
+  
+  string_repeated <- rep(string,9)
+  number_words <- c("one","two","three","four","five","six","seven","eight","nine")
+  number_locs <- str_locate(string_repeated,number_words) |> as_tibble() 
+  number_locs$numberword <- number_words
+  number_locs$number <- seq(1,9) |> as.character()
+  
+  first_num_to_replace <-
+    number_locs |> filter(start == min(start, na.rm = TRUE)) 
+  last_num_to_replace <-
+    number_locs |> filter(end == max(end, na.rm = TRUE)) 
+  
+  if (length(first_num_to_replace) != 0) {
+    string <-
+      str_replace(string,
+                  first_num_to_replace$numberword,
+                  first_num_to_replace$number)
+  }
+  if (length(last_num_to_replace) != 0) {
+    string <-
+      str_replace_all(string,
+                      last_num_to_replace$numberword,
+                      last_num_to_replace$number)
+  }
   
   return(string)
 }
 
-test <- "onetwo34"
-
-words_replaced <- replace_words_with_numbers(data_pt2)
+words_replaced <- sapply(data_pt2$data,replace_words_with_numbers) |> 
+  unlist()
 
 #get the numbers
 numbers_only_pt2 <- data.frame(numbers = stringr::str_extract_all(words_replaced,"\\d+") |> 
